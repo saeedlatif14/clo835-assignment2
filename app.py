8965750430f6b6302b2cd8254b5931a655863655
EC2 Default User <ec2-user@ip-172-31-64-59.ec2.internal>
2025-03-01 17:02:25 +0000

    final upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "password"  # Fixed typo in default password
 DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
+COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "blue"
 DBPORT = int(os.environ.get("DBPORT", "3306"))  # Default MySQL port is 3306
 
 # Create a connection to the MySQL database
